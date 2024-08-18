e748fe701ab17c0471c0dacdbd0b124c70354c57
Ahmed <elzek989@gmail.com>
2024-08-18 22:57:29 +0300

    Finish AddTaskFragment Design

---
+++ app/src/main/java/com/example/route/todoapp/home/fragments/add_task/AddTaskFragment.kt
@@ -0,0 +1,22 @@
+package com.example.route.todoapp.home.fragments.add_task
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.route.todoapp.R
+
+
+class AddTaskFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_task, container, false)
+    }
+
+}
